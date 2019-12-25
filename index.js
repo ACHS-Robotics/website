eeca48be16f866327ccb83ab608dc5fea7d80abb
Scorps6934 <36343362+Scorps6934@users.noreply.github.com>
2019-12-24 17:48:55 -0800

    Fixed/ improved navbar

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 var tl=gsap.timeline(); 
-var controller=new ScrollMagic.Controller();
 tl.from("#robot-background",{
     webkitFilter: 'blur(6px)',filter: 'blur(6px)',duration: 4
 });
-tl.to("#cam-logo-picture",{
-    opacity:0.75, duration:3, ease: "power1",y:-5
-},"-=3");
 var tween1=gsap.from(".about-container", {
     opacity:0, y:200, duration: 1, ease: "power1" 
 });
 var tween2=gsap.from(".first-container", {
     opacity:0, x:-200, duration: 1, ease: "power1" 
 });
+var controller=new ScrollMagic.Controller();
 var scene=new ScrollMagic.Scene({
       triggerElement:".about-container",
       triggerHook: 1
@@ -27,3 +24,43 @@ var scene=new ScrollMagic.Scene({
 .setTween(tween2)
 .addIndicators()
 .addTo(controller);
+/*function that pulls down the dropdown*/
+var dropdown=gsap.timeline();
+dropdown.paused(true);
+dropdown.from("nav ul li ul",{
+    y:-20, duration:1, ease:"power1"
+});
+dropdown.to("nav ul li ul",{
+    display:"block", duration:1, opacity:1
+},"-=1");
+/*function that pulls up the dropdown*/
+var pullup=gsap.timeline();
+pullup.paused(true);
+pullup.to("nav ul li ul",{
+    y:-20, duration:0.5, ease:"power1"
+});
+pullup.to("nav ul li ul",{
+    display:"none", opacity:0, duration:0.5
+});
+/*toggles if it is up or down*/
+var extendedDown=false;
+function toggleNavbarRight(){
+    if(extendedDown){
+        pullup.play(0);
+        console.log("pullup");
+    }else{
+        console.log("dropdown");
+        dropdown.play(0);
+    }
+}
+const navbarRight= document.querySelector(".memory-pulldown");
+navbarRight.addEventListener("click",(e)=>{
+    if(dropdown.isActive()||pullup.isActive()){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return false;
+    }
+    toggleNavbarRight();
+    extendedDown=!extendedDown;
+    console.log("var is " + extendedDown);
+});
